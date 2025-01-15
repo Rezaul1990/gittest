d72533caab822efa134ed2e01e8c153656d94bd3
Rezaul <rezaulpust.19.cse@gmail.com>
2025-01-15 12:40:21 +0600

    vercel push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold">Home Page</h1>
+      <h1 className="text-2xl font-bold">Home Page update</h1>
     </div>
   );
 }
